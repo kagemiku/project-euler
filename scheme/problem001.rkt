ddeaaefdeab52a5a08c8125967005cb68b4c2e2b
KAGE <kagemiku@gmail.com>
2016-12-04 22:18:58 +0900

    Use range function

--- scheme/problem001.rkt
+++ scheme/problem001.rkt
@@ -3,14 +3,9 @@
 (define (divisor? x y)
   (= (remainder x y) 0))
 
-(define (range-list bgn end)
-  (if (>= bgn end)
-    '()
-    (cons bgn (range-list (+ bgn 1) end))))
-
 (define (solver num)
   (apply + (filter
              (lambda (x) (or (divisor? x 3) (divisor? x 5)))
-             (range-list 2 num))))
+             (range 2 num))))
 
 (solver 1000)
