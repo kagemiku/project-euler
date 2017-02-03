e0baa55c1bd28171fc087f79647c26e9abfa18ea
KAGE <kagemiku@gmail.com>
2017-02-03 20:47:25 +0900

    Improve problem004

--- scheme/problem004.rkt
+++ scheme/problem004.rkt
@@ -1,24 +1,23 @@
 #lang racket
 
-(define (number->list number)
-  (string->list (number->string number)))
+(define (integer-reverse num)
+  (do ((num num (quotient num 10))
+       (r 0 (+ (* 10 r) (modulo num 10))))
+    ((zero? num) r)))
 
 (define (palindromic? num)
-  (let loop((ls0 (number->list num)) (ls1 (reverse (number->list num))) (cnt (truncate (/ (length (number->list num)) 2))))
-    (cond
-      ((<= cnt 0) #t)
-      ((eqv? (car ls0) (car ls1)) (loop (cdr ls0) (cdr ls1) (- cnt 1)))
-      (else #f))))
+  (= num (integer-reverse num)))
 
-(define (gen-muls max-num)
+(define (gen-muls max-num min-num)
   (let loop((x max-num) (y max-num) (ret '()))
     (cond
-      ((= y 0) ret)
-      ((= x 0) (loop max-num (- y 1) ret))
-      (else (loop (- x 1) y (cons (* x y) ret))))))
+      ((< y min-num) ret)
+      ((< x y) (loop max-num (- y 1) ret))
+      ((palindromic? (* x y)) (loop max-num (- y 1) (cons (* x y) ret)))
+      (else (loop (- x 1) y ret)))))
 
-(define (solver max-num)
-  (apply max (filter palindromic? (gen-muls max-num))))
+(define (solver max-num min-num)
+  (apply max (gen-muls max-num min-num)))
 
-(solver 999)
+(solver 999 100)
 
